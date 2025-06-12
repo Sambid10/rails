936c81db095fa54079cd4c1b054d64270f0c34f0
Sambid10 <sambidshk10@gmail.com>
2025-06-12 07:01:52 +0000

    till chapter-6

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def about
+
+  end
+
+  def random
+    @idea=Idea.all.sample
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'ideas#index'
+  root "ideas#index"
 
-  resources:ideas
+  resources :ideas
+
+  get "about" =>"static_pages#about"
+  get "random" => "static_pages#random"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
